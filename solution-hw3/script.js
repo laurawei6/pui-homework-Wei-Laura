d46c55dd6aaa570165ee0efd1be389a13ebd485a
Laura Wei <laura.wei@visgence.com>
2023-09-20 14:13:31 -0400

    Update script.js

--- solution-hw3/script.js
+++ solution-hw3/script.js
@@ -1,11 +1,11 @@
 // glazing options for buns
-let glazing = {
+const glazing = {
     options: ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"],
     priceAdaptation: ["+ $0.00", "+ $0.00", "+ $0.50", "+ $1.50"],
 }
 
 // pack size options for buns
-let packSize = {
+const packSize = {
     options: [1, 3, 6, 12],
     priceAdaptation: ["*1", "*3", "*5", "*10"],
 }
@@ -14,5 +14,7 @@ let packSize = {
 let glazingDropdown = document.querySelector("#glazing");
 
 for (let option of glazing.options){
-    console.log(option);
+    let newOption = document.createElement("option");
+    newOption.setAttribute("value", option);
+    glazingDropdown.appendChild(newOption);
 }
