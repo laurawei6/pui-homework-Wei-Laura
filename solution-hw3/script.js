518c5d14358729e602dec0a2cf1f59e8f2c68c81
Laura Wei <laura.wei@visgence.com>
2023-09-20 11:40:43 -0400

    Update script.js

---
+++ solution-hw3/script.js
@@ -0,0 +1,11 @@
+let glazing = {
+    options: ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"],
+    price-adaptation: ["+ $0.00", "+ $0.00", "+ $0.50", "+ $1.50"],
+}
+
+let packSize = {
+    options: [1, 3, 6, 12],
+    price-adaptation: ["* 1", "* 3", "* 5", "* 10"],
+}
+
+console.log(glazing.options[1]);
