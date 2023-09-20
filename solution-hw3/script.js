0f53a68ef04f2c5e74343a6eb53596e6c1857286
Laura Wei <laura.wei@visgence.com>
2023-09-20 14:36:51 -0400

    Update script.js

--- solution-hw3/script.js
+++ solution-hw3/script.js
@@ -1,10 +1,10 @@
-// glazing options for buns
+// glazing options for rolls
 const glazing = {
     options: ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"],
     priceAdaptation: ["+ $0.00", "+ $0.00", "+ $0.50", "+ $1.50"],
 }
 
-// pack size options for buns
+// pack size options for rolls
 const packSize = {
     options: [1, 3, 6, 12],
     priceAdaptation: ["*1", "*3", "*5", "*10"],
@@ -12,9 +12,21 @@ const packSize = {
 
 // dropdown options
 let glazingDropdown = document.querySelector("#glazing");
+let packSizeDropdown = document.querySelector("#pack-size");
 
 for (let option of glazing.options){
     let newOption = document.createElement("option");
     newOption.setAttribute("value", option);
+    newOption.textContent = option;
     glazingDropdown.appendChild(newOption);
 }
+
+for (let option of packSize.options){
+    let newOption = document.createElement("option");
+    newOption.setAttribute("value", option);
+    newOption.textContent = option;
+    packSizeDropdown.appendChild(newOption);
+}
+
+// roll prices
+let defaultPrice = 2.49
