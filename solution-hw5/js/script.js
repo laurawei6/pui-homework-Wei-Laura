2ce8b454f90bf4177e256caeb56971d680e19500
Laura Wei <laura.wei@visgence.com>
2023-10-05 15:16:28 -0400

    sol5

---
+++ solution-hw5/js/script.js
@@ -0,0 +1,97 @@
+// detail page updates
+const cart = [];
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get('roll');
+
+// glazing options for rolls
+const glazing = {
+    options: ["Keep original", "Sugar milk", "Vanilla milk", "Double chocolate"],
+    priceAdaptation: [0, 0, 0.5, 1.5],
+}
+
+// pack size options for rolls
+const packSize = {
+    options: [1, 3, 6, 12],
+    priceAdaptation: [1, 3, 5, 10],
+}
+
+// dropdown options
+let glazingDropdown = document.querySelector("#glazing");
+let packSizeDropdown = document.querySelector("#pack-size");
+
+for (let i=0; i < glazing.options.length; i++){
+    let newOption = document.createElement("option");
+    newOption.setAttribute("value", glazing.priceAdaptation[i]);
+    newOption.textContent = glazing.options[i];
+    glazingDropdown.appendChild(newOption);
+}
+
+for (let i=0; i < packSize.options.length; i++){
+    let newOption = document.createElement("option");
+    newOption.setAttribute("value", packSize.priceAdaptation[i]);
+    newOption.textContent = packSize.options[i];
+    packSizeDropdown.appendChild(newOption);
+}
+
+// roll prices
+let basePrice = rolls[rollType].basePrice;
+let totalPriceDetail = document.querySelector(".total.detail");
+let glazingPrice = 0;
+let packPrice = 1;
+let totalItemPrice;
+totalPriceDetail.innerHTML = "$" + basePrice;
+
+function glazingChange(element) {
+    // get value of selected glazing option
+    glazingPrice = parseFloat(element.value);
+    
+    // update the price
+    // the formula to update the price
+    totalItemPrice = ((basePrice + glazingPrice) * packPrice).toFixed(2);
+    /* used this stackoverflow thread to round to two decimal places: 
+    https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary */
+    
+    // the formula to update the HTML to the new price
+    totalPriceDetail.innerHTML = "$" + String(totalItemPrice);
+}
+
+function packChange(element) {
+    // get value of selected pack size option
+    packPrice = parseInt(element.value);
+
+    // update the price
+    // the formula to update the price
+    totalItemPrice = ((basePrice + glazingPrice) * packPrice).toFixed(2);
+    /* used this stackoverflow thread to round to two decimal places: 
+    https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-decimal-places-if-necessary */
+
+    // the formula to update the HTML to the new price
+    totalPriceDetail.innerHTML = "$" + String(totalItemPrice);
+}
+
+// Update the header text
+const headerElement = document.querySelector('.detail-header');
+headerElement.innerText = rollType + " Cinnamon Roll";
+
+// Update the image
+const detailImage = document.querySelector('#roll-detail-page');
+let rollImage = rolls[rollType].imageFile;
+detailImage.src = '../assets/products/' + rollImage;
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
+// add to cart
+let addToCart = document.querySelector("button");
+addToCart.addEventListener("click", function() {
+    let roll = new Roll(rollType, glazingDropdown.value, packSizeDropdown.value, basePrice);
+    cart.push(roll);
+    console.log(cart);
+})
