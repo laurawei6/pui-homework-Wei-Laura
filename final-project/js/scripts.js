b222e5283f05dcef091401e2ed93d92265d8ebba
Laura Wei <laura.wei@visgence.com>
2023-11-01 11:36:40 -0400

    script

---
+++ final-project/js/scripts.js
@@ -0,0 +1,14 @@
+function clickedHome() {
+    var home = document.getElementById("home");
+    home.style.background="#FFFFFF"
+}
+
+function clickedCredits() {
+    var songCreds = document.getElementById("song-credits");
+    songCreds.style.background="#FFFFFF"
+}
+
+function clickedThemes() {
+    var themes = document.getElementById("themes");
+    themes.style.background="#FFFFFF"
+}
