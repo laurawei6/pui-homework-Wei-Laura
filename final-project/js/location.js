a005683c50874985536397911f54b1b8595dc71c
Laura Wei <laura.wei@visgence.com>
2023-12-05 21:27:16 -0500

    Update location.js

--- final-project/js/location.js
+++ final-project/js/location.js
@@ -5,7 +5,14 @@ cityDropdown.addEventListener("click", determineWeather);
 function determineWeather() {
     if (cityDropdown.value == "Pittsburgh") {
         console.log("pittsburgh");
+        // lat = 
     } else if (cityDropdown.value == "Tokyo") {
         console.log("Tokyo");
+    }
+}
+
+class City(lat, long){
+    constructor(lat, long){
+        
     }
 }
