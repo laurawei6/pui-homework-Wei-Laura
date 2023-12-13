fa014293589df07d7e1b3a4bd1a208b13e43c9e0
Laura Wei <laura.wei@visgence.com>
2023-12-13 10:33:44 -0500

    remove

--- final-project/js/scripts.js
+++ final-project/js/scripts.js
@@ -1,15 +1,4 @@
 
-
-// left side info
-// home information
-// const home = document.getElementById("home");
-// const homeIcon = document.querySelector("#home img");
-
-// credits information
-
-
-// themes information
-
 // selected navigation item
 const selectedOrNo = document.querySelectorAll("ul div");
 for (let navDiv of selectedOrNo){
@@ -23,25 +12,4 @@ for (let navDiv of selectedOrNo){
     }
 }
 
-// icons/Left Banner/Home - Filled.svg
-// const selectedImg = document.querySelector(".selected img");
-// console.log(selectedImg);
-// selectedImg.src = ''
-// detailImage.src = '../assets/products/' + rollImage
-
-
-// function clickedHome() {
-//     const home = document.getElementById("home");
-//     const homeIcon = document.querySelector("#home img");
-//     console.log(homeIcon);
-// }
-
-// function clickedCredits() {
-//     const songCreds = document.getElementById("song-credits");
-//     songCreds.style.background="#FFFFFF"
-// }
-
-// function clickedThemes() {
-//     const themes = document.getElementById("themes");
-//     themes.style.background="#FFFFFF"
-// }
+import {Howl, Howler} from 'howler';
